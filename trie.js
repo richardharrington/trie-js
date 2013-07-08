a6317fc31e1b36202c42cc27b3d5940e85f752db
Doron Roberts-Kedes <doronr@sas.upenn.edu>
2013-07-08 18:04:33 -0400

    Update trie.js

--- trie.js
+++ trie.js
@@ -1,15 +1,17 @@
 function Trie(){
-  this.name = "trie";
 	this.children = {};
 	this.count = 0;
+	this.letter;
 }
 	
-
-
 Trie.prototype = {
-	insert: function(word) {
+	insert: function(word, letter) {
 		//console.log(word);
 		//console.log("inserting");
+		if (letter === undefined){
+			letter = "";
+		}
+		this.letter = letter;
 		if (word.length === 0){
 			this.count++;
 			return;
@@ -19,28 +21,69 @@ Trie.prototype = {
 		if (this.children[firstLetter] == undefined) {
 			this.children[firstLetter] = new Trie();
 		}
-		this.children[firstLetter].insert(restOfWord);
+		this.children[firstLetter].insert(restOfWord, firstLetter);
 	},
-	lookupCount: function(word) {
-		if (word.length === 0){
-			return this.count
+	getCount: function(word) {
+		var c = this.getNode(word);
+		if (c === undefined) {
+			return 0;
+		}
+		return this.getNode(word).count;
+	},
+	getSuffix: function() {
+		var list = [];
+		for (var key in this.children) {
+			list = list.concat(this.children[key].getSuffix());
+		}
+		for (var i in list) {
+			list[i][0] = this.letter + list[i][0];
+		}
+		if (this.count > 0) {
+			list.push([this.letter, this.count]);
+		}
+		return list;
+	},
+	getNode: function(word){
+		if (word.length === 0) {
+			return this;
 		}
 		var firstLetter = word[0];
 		var restOfWord = word.substring(1);
-		if (this.children[firstLetter] == undefined){
-			return 0;
+		if (this.children[firstLetter] === undefined){
+			return undefined;
+		} else {
+			return this.children[firstLetter].getNode(restOfWord);
 		}
-		return this.children[firstLetter].lookupCount(restOfWord);
+	},
+	getWordsFromPrefix: function(prefix){
+		var subTree = this.getNode(prefix);
+		var endings = subTree.getSuffix();
+		var words = [];
+					// console.log(endings)
+		for (i in endings) {
+			words.push([prefix.substring(0,prefix.length-1) + endings[i][0], endings[i][1]]);
+		}
+		return words;
+	},
+	getSortedWordsFromPrefix: function(prefix){
+		var words = this.getWordsFromPrefix(prefix);
+		return words.sort(function(a,b) {return b[1] - a[1]});
 	}
-}
 }
 
 var myTrie = new Trie();
-myTrie.insert("apple");
-myTrie.insert("apple");
-myTrie.insert("apple");
-myTrie.insert("apli");
+var words = ["", "a", "at","ads","adva","but","at","but","but"];
+for (var i in words) {
+	myTrie.insert(words[i]);
+}
 
-console.log(myTrie.lookupCount("apple"));
-console.log(myTrie.lookupCount("apli"));
-console.log(myTrie.lookupCount("appl"));
+// a = ["a", 2];
+// b = ["b", 0];
+// c = ["c", 1];
+// d = ["d", 5];
+// e = ["e", 9];
+
+// var list = [a,b,c,d,e];
+// console.log(list.sort(function(a,b) {return a[1] - b[1]}));
+
+console.log(myTrie.getSortedWordsFromPrefix(""));
