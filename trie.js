d4b126427e6a48e2dcec6c9d1b4224a9f3ae329d
Richard Harrington <harrington.richard@gmail.com>
2013-07-09 01:30:38 -0400

    extensively reworked, nodes now do not contain information about their letters. needs testing

--- trie.js
+++ trie.js
@@ -1,82 +1,89 @@
+var splitWord = function(word, emptyCb, nonEmptyCb) {
+    if (word.length === 0) {
+        return emptyCb();
+    }
+    else {
+        return nonEmptyCb(word[0], word.substring(1));
+    }
+}
+
 function Trie(){
-	this.children = {};
-	this.count = 0;
-    this.letter = "";
+    // will be set with properties "val" and/or "children"
 }
 	
-var getFirstLetter = function(word, emptyCallback, nonEmptyCallback) {
-    if (word.length === 0) {
-        return emptyCallback();
-    }
-    return nonEmptyCallback(word[0], word.substring(1));
-};
-
 Trie.prototype = {
-    insertWord: function(word, letter) {
-        if (letter !== undefined) {
-            this.letter = letter;
-        }
+
+    getNode: function(word) {
         var self = this;
-        getFirstLetter(word,
-                function(){
-                    self.count++;
-                },
-                function(firstLetter, restOfWord) {
-                    self.children[firstLetter] = self.children[firstLetter] || new Trie();
-                    self.children[firstLetter].insertWord(restOfWord, firstLetter);
-                });
-	},
-    insertWords: function(words) {
-        for (var i = 0, len = words.length; i < len; i++) {
-            this.insertWord(words[i]);
+        return splitWord(word, function() {
+            return self;
+        }, function(firstLetter, restOfWord) {
+            return self.children[firstLetter] && self.children[firstLetter].getNode(restOfWord);
+
+    set: function(word, val) {
+        if (typeof word === "object") {
+            for (var key in word) {
+                this.set(key, word[key]);
+            }
+        }
+        else {
+            var self = this;
+            splitWord(word, function() {
+                self.val = val;
+            }, function(firstLetter, restOfWord) {
+                self.children = self.children || {};
+                self.children[firstLetter] = self.children[firstLetter] || new Trie();
+                self.children[firstLetter].set(restOfWord, val);
+            });
         }
     },
-	getNode: function(word){
-        var self = this;
-        return getFirstLetter(word,
-                function() {
-                    return self;
-                },
-                function(firstLetter, restOfWord) {
-                    if (self.children[firstLetter] !== undefined) {
-                        return this.children[firstLetter].getNode(restOfWord);
-                    }
-                });
-	},
-	getSuffix: function() {
-		var list = [];
-		for (var key in this.children) {
-			list = list.concat(this.children[key].getSuffix());
-		}
-		for (var i = 0, len = list.length; i < len; i++) {
-			list[i][0] = this.letter + list[i][0];
-		}
-		if (this.count > 0) {
-			list.push([this.letter, this.count]);
-		}
-		return list;
-	},
+
+    get: function(word) {
+        var node = this.getNode(word);
+        return node && node.val;
+    },
+    
+	getSuffixes: function() {
+        var mainList = [];
+        var list;
+        var i, len;
+        for (var letterKey in this.children) {
+           list = this.children[letterKey].getSuffixes();
+           for (i = 0, len = list.length; i < len; i++) {
+               list[i] = letterKey + list[i];
+           }
+           mainList = mainList.concat(list);
+        }
+        return mainList;
+    },
+
 	getWordsFromPrefix: function(prefix){
 		var subTree = this.getNode(prefix);
-		var endings = subTree.getSuffix();
+		var suffixes = subTree.getSuffixes();
 		var words = [];
-		for (i in endings) {
-			words.push([prefix.substring(0,prefix.length-1) + endings[i][0], endings[i][1]]);
+		for (var i = 0, len = suffixes.length; i < len; i++) { 
+			words.push(prefix + suffixes[i]);
 		}
 		return words;
 	},
-	getSortedWordsFromPrefix: function(prefix){
-		var words = this.getWordsFromPrefix(prefix);
-		return words.sort(function(a,b) {return b[1] - a[1]});
-	},
-	getCount: function(word) {
-		var c = this.getNode(word);
-		if (c === undefined) {
-			return 0;
-		}
-		return this.getNode(word).count;
-	}
 
+	getSortedWordsFromPrefix: function(prefix){
+		return this.getWordsFromPrefix(prefix).sort();
+	}
 };
-//module.exports = Trie;
+
+var myTrie = new Trie();
+var words = ["", "a", "at","ads","adva","but","at","but","but"];
+myTrie.insertWords(words);
+
+// a = ["a", 2];
+// b = ["b", 0];
+// c = ["c", 1];
+// d = ["d", 5];
+// e = ["e", 9];
+
+// var list = [a,b,c,d,e];
+// console.log(list.sort(function(a,b) {return a[1] - b[1]}));
+
+console.log(myTrie.getSortedWordsFromPrefix(""));
 
